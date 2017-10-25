177b25ed8c9076e2945a38e17de407aa0841fd2f
Stephen Chatelain <stephen.chatelain@campingworld.com>
2017-10-25 15:05:41 -0400

    Log text response on error

--- src/Broker.php
+++ src/Broker.php
@@ -218,6 +218,7 @@ class Broker
         list($contentType) = explode(';', curl_getinfo($ch, CURLINFO_CONTENT_TYPE));
 
         if ($contentType != 'application/json') {
+            error_log($response);
             $message = 'Expected application/json response, got ' . $contentType;
             throw new Exception($message);
         }
